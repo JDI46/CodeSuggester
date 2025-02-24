871e89f730b54e69ba12ae4f1e9643a3a931026f
Xander Whitby <xwhitby20@gmail.com>
2025-02-23 19:24:08 -0700

    first workout app commit

---
+++ workout_manager.py
@@ -0,0 +1,21 @@
+#parent class to store workouts
+class WorkoutStorage:
+#use hash set for WorkoutStorage
+    pass
+
+
+
+
+class GetWorkout:
+    def __init__(self):
+        
+
+    def recieved_workout(self):
+        pass
+
+    def difficulty_check():
+        pass
+
+
+class WeeklyResults:
+    pass
