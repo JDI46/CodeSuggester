a7693589bc792ec06e0078c2f52b24527e424af1
Xander Whitby <xwhitby20@gmail.com>
2025-02-24 14:14:01 -0700

    working on exercise class

--- workout_manager.py
+++ workout_manager.py
@@ -1,35 +1,40 @@
 #parent class to store workouts
-class WorkoutStorage:
-#use hash set for WorkoutStorage
-    pass
+# class WorkoutStorage:
+# #use hash set for WorkoutStorage
+#     pass
 
-
-
-
-class GetWorkout:
-    def __init__(self):
-        
-
-    def recieved_workout(self):
-        pass
-
-    def difficulty_check():
-        pass
-
+#
+#
+#
+# class GetWorkout:
+#     def __init__(self):
+#
+#
+#     def recieved_workout(self):
+#         pass
+#
+#     def difficulty_check():
+#         pass
+#
 class Exercise:
-    def __init__():
-        pass
+    def __init__(self, exercise, reps):
+        self.excercise = exercise
+        self.reps = reps
+        self.storage = []
+    
+    def enter_workout(self):
+        workout
 
-class UserProfile:
-    pass
-
-class ProgressTracker:
-    pass
-
-class Suggestions:
-    pass
-
-
-
-class WeeklyResults:
-    pass
+# class UserProfile:
+#     pass
+#
+# class ProgressTracker:
+#     pass
+#
+# class Suggestions:
+#     pass
+#
+#
+#
+# class WeeklyResults:
+#     pass
