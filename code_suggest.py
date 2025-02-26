34842b3c9ed968096866bfffbc5665b54b02f6da
Xander Whitby <xwhitby20@gmail.com>
2025-02-26 09:18:36 -0700

    saved all files

--- code_suggest.py
+++ code_suggest.py
@@ -1,43 +1,13 @@
-# class GetWorkout:
-#     def __init__(self):
-#
-#
-#     def recieved_workout(self):
-#         pass
-#
-#     def difficulty_check():
-#         pass
-#
-class Suggest:
-    def __init__(self, code_block, goal):
-        self.day = day
-        self.excercise = exercise
-        self.sets = sets
-        self.reps = reps
-        self.storage = {}
+
+class Prompt:
+    def __init__(self, prompt, code_block, goal):
+        self.prompt = prompt
+        self.code_block = code_block
+        self.goal = goal
     
-    def enter_workout(self):
-        workout = f"Workout: {self.exercise}; Sets: {self.sets}; Reps: {self.reps}"
-        return self.storage[self.day] == workout 
+    def enter_prompt(self):
+        pass
 
-    def find_workouts(self):
-    #looks for key and returns the string
-    find_key = self.storage.get(self.day)
-    return find_key
+    def desired_outcome(self):
+        pass
 
-# class UserProfile:
-#     pass
-#
-# class ProgressTracker:
-#     pass
-#
-# class Suggestions:
-#     pass
-#
-#
-#
-# class WeeklyResults:
-#     pass
-
-workout1 = Exercise("Biceps", 3, 12)
-print(workout1)
